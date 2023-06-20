1b9f586655b532ed4a9e838ef409367f0b54a772
UmairNasirAhmad29 <ahmed.riaz@esparksinc.com>
2023-06-20 17:30:35 +0500

    Adding quantity counter in mini cart

---
+++ Esparksinc/QtyCounterMiniCart/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/minicart/item/default.html': 'Esparksinc_QtyCounterMiniCart/template/minicart/item/default.html',
+            qtychange: 'Esparksinc_QtyCounterMiniCart/js/qtychange'
+        }
+    }
+};
